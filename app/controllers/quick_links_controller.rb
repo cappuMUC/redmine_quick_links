3a4af023268b77e342ef5d78378ca3f23d7782c2
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-05-22 14:23:02 +0200

    added methods for record creation

--- app/controllers/quick_links_controller.rb
+++ app/controllers/quick_links_controller.rb
@@ -2,6 +2,25 @@ class QuickLinksController < ApplicationController
   layout 'admin'
   self.main_menu = false
 
-  
 
+  def new
+    @quick_link = QuickLink.new
+  end
+
+  def create
+    params.permit!
+    @quick_link = QuickLink.new( quick_link_params )
+    if @quick_link.save
+      redirect_to quick_links_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+
+  def quick_link_params
+    puts
+    params.require( :quick_link ).permit( :label, :description, :link, :project_id, :role_id, :icon_class )
+  end
 end
