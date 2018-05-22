94f70f7cd691f920845305a55247a2ed3a542c1c
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-05-22 09:52:36 +0200

    added 'Quick Links' item to admin menu

--- init.rb
+++ init.rb
@@ -6,4 +6,12 @@ Redmine::Plugin.register :redmine_quick_links do
   version '0.0.1'
   url 'https://github.com/cappumuc/redmine_quick_links'
   author_url ''
+
+
+  menu :admin_menu,
+    :shortcuts,
+    { :controller => 'quick_links' },
+    :caption => "Quick Links",
+    :after => :enumerations,
+    :html => { :class => 'icon icon-summary' }
 end
