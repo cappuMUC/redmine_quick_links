b3d460d31d637bb60535b3c4a42ea7ef670afefe
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-05-22 07:00:10 +0200

    added init.rb file

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_quick_links do
+  name 'Redmine Quick Links plugin'
+  author 'Bernhard Rohloff'
+  description 'Shows custom links for users based on projects and roles'
+  version '0.0.1'
+  url 'https://github.com/cappumuc/redmine_quick_links'
+  author_url ''
+end
