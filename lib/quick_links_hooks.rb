2a251411c12587afcde49547cfc5f924724219f2
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-05-24 17:55:33 +0200

    simplified partial view
    moved part of logic to the Hooks class

--- lib/quick_links_hooks.rb
+++ lib/quick_links_hooks.rb
@@ -1,6 +1,25 @@
 module RedmineQuickLinks
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_welcome_index_right,
-      :partial => 'quick_links/quick_link_box'
+    def view_welcome_index_right( context = {})
+      if User.current.logged? && has_links_to_show?(context)
+        context[:controller].send(
+          :render_to_string, {
+          :partial => 'quick_links/quick_link_box',
+          :locals => context
+        })
+      end
+    end
+
+    private
+    def has_links_to_show?(context)
+      project_links = []
+      User.current.project_ids.each do |project|
+        roles = User.current.membership(project).roles.to_a
+        project_links.concat QuickLink.where( :project_id => project, :role_id => roles )
+      end
+      context[:project_links] = project_links
+      context[:public_links] = QuickLink.where( :project_id => 0, :role_id => 0 )
+      context[:public_links].any? || context[:project_links].any?
+    end
   end
 end
