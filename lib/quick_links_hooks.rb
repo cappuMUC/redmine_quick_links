a114a1280d2109e9b8295615a76f0058312e02cf
Bernhard Rohloff <rohloff.bernhard@gmail.com>
2018-06-07 15:51:50 +0200

    added quick links box to project overview page

--- lib/quick_links_hooks.rb
+++ lib/quick_links_hooks.rb
@@ -3,8 +3,9 @@ module RedmineQuickLinks
 
     def view_welcome_index_right( context = {})
       if User.current.logged?
+        context[:show_project_headings] = true
         collect_global_links_for_context(context)
-        collect_project_links_for_context(context)
+        collect_global_project_links_for_context(context)
         if context_has_links_to_show?(context)
           context[:controller].send(
           :render_to_string, {
@@ -15,6 +16,22 @@ module RedmineQuickLinks
         end
       end
 
+      def view_projects_show_right( context = {})
+        if User.current.logged? && User.current.membership(context[:project])
+          context[:show_project_headings] = false
+          context[:public_links] = []
+          context[:project_links] = []
+          collect_project_links_for_context(context)
+          if context_has_links_to_show?(context)
+            context[:controller].send(
+            :render_to_string, {
+              :partial => 'quick_links/quick_link_box',
+              :locals => context
+              })
+            end
+          end
+        end
+
       private
 
       def collect_global_links_for_context(context)
@@ -23,6 +40,14 @@ module RedmineQuickLinks
       end
 
       def collect_project_links_for_context(context)
+        project_links = []
+        roles = User.current.membership(context[:project]).roles.to_a
+        project_links.concat QuickLink.where( :project_id => context[:project], :role_id => roles )
+        project_links.concat QuickLink.where( :project_id => context[:project], :role_id => nil )
+        context[:project_links] = project_links.sort_by{|e| e[:position]}
+      end
+
+      def collect_global_project_links_for_context(context)
         project_links = []
         User.current.project_ids.each do |project|
           roles = User.current.membership(project).roles.to_a
